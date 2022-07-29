8587ad8d699026ba122b9a130ff31039d12de8cf
Iced Coffee <jackbphillips@gmail.com>
2022-07-29 18:38:17 -0400

    added generatePassword function

--- Assests/script.js
+++ Assests/script.js
@@ -4,6 +4,17 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+
+  function generatePassword() {
+    var length = 8,
+        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+        retVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return retVal;
+}
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
