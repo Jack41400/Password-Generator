4a8072c80cb7882bc084ff46f18ca0d967819e1e
Iced Coffee <jackbphillips@gmail.com>
2022-07-30 10:30:00 -0400

    added to js file

--- Assests/script.js
+++ Assests/script.js
@@ -6,6 +6,13 @@ function writePassword() {
   var password = generatePassword();
 
   function generatePassword() {
+
+    enter = parseInt(prompt("How many characters would you like your password? Choose between 8 and 128"));
+    if (!enter) {
+      alert("This needs a value");
+  }  else if (enter < 8 || enter > 128) {
+      enter = parseInt(prompt("This Value MUST be between 8 and 128!!!"));
+
     var length = 8,
         charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
         retVal = "";
@@ -13,13 +20,18 @@ function writePassword() {
         retVal += charset.charAt(Math.floor(Math.random() * n));
     }
     return retVal;
-}
 
+  }
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
+  }
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+var lowerSetNoSpecial = "abcdefghijklmnopqrstuvwxyz0123456789";
+var upperSetNoSpecial = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+var mixedSetNoSpecial = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
